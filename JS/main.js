61f76b1a3161e12c1f0f78ddad8cb1f6a08aecd3
SreehariVinayan <sreeharivinayan06@gmail.com>
2025-02-17 21:07:08 +0530

    Delete JS directory

--- JS/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("search").addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault(); // Prevents form submission (if inside a form)
-        alert("You searched for: " + this.value); // Replace this with your function
-    }
-});
