274fe2f4a22234de08c933e6df3f32a8e18daf72
SreehariVinayan <sreeharivinayan06@gmail.com>
2025-02-21 00:16:23 +0530

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,29 @@
-document.getElementById("search").addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault(); // Prevents form submission (if inside a form)
-        alert("You searched for: " + this.value); // Replace this with your function
+
+window.addEventListener("scroll", function() {
+    h2 = document.getElementById("h2");
+    tab=document.getElementById("reviews");
+    if(window.scrollY>=100) {
+        tab.style.opacity=1;
+        h2.style.opacity= 1; 
+        tab.style.transform = "translateY(0px)";
+        h2.style.transform = "translateY(0)";
     }
+    else{
+        tab.style.opacity=0
+        tab.style.transform = "translateY(20px)";
+        h2.style.opacity= 0; 
+        h2.style.transform = "translateY(20px)";
+    }
+});
+
+
+document.querySelectorAll('.reviews img').forEach(img => {
+    img.addEventListener('click', function() {
+        document.getElementById('fullscreen-img').src = this.src;
+        document.getElementById('fullscreen').style.display = 'flex';
+    });
+});
+
+document.getElementById('fullscreen').addEventListener('click', function() {
+    this.style.display = 'none';
 });
